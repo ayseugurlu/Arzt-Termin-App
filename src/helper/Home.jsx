6707ab7d76104a35228facee5317ad9d464940bd
ayseugurlu <ayseugurlu9135@gmail.com>
2024-07-17 12:55:15 +0200

    "Added logo image and header to Home component"

--- src/helper/Home.jsx
+++ src/helper/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import HastaListe from '../components/HastaListe'
 import Data from "./data"
+import logo from "../helper/logo.png"
 
 const Home = () => {
 
@@ -10,6 +11,13 @@ const Home = () => {
     // 
   return (
     <div>
+
+        <div style={{display:"flex", justifyContent:"flex-start", alignItems:"center" ,gap:"1rem"}}>
+
+          <img style={{width:"100px", height:"100px"}} src={logo} alt="" />
+          <h1 style={{fontFamily:"monospace"}}>MEDICAL CENTER</h1>
+
+        </div>
         <HastaListe liste={liste} setListe={setListe}/>
         
     </div>
