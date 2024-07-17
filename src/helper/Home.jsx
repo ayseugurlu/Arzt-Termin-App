7cb576848ab4361824d64aca5b02148a1eddd159
ayseugurlu <ayseugurlu9135@gmail.com>
2024-07-17 13:02:07 +0200

    update

--- src/helper/Home.jsx
+++ src/helper/Home.jsx
@@ -8,11 +8,11 @@ const Home = () => {
 
     const [liste,setListe]=useState(JSON.parse(localStorage.getItem("hastaListesi")) || Data )
 
-    // 
+    
   return (
     <div>
 
-        <div style={{display:"flex", justifyContent:"flex-start", alignItems:"center" ,gap:"1rem"}}>
+        <div style={{display:"flex", justifyContent:"center", alignItems:"center" ,gap:"2rem" , margin:"2rem 1rem" }}>
 
           <img style={{width:"100px", height:"100px"}} src={logo} alt="" />
           <h1 style={{fontFamily:"monospace"}}>MEDICAL CENTER</h1>
